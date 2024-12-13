89f5411dfb0cc9e8c065d7ff5f63916cd011a754
Anthony Morel <anthony_morel@hotmail.com>
2024-12-13 10:00:23 -0400

    Adsense

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import LotteryCards from "./components/LotteryCards";
 function App() {
   return (
     <div className="bg-gray-100 min-h-screen">
-      <h1 className="text-3xl font-bold text-center my-6">Resultados de Loterías</h1>
+      <h1 className="text-3xl font-bold text-center my-6">Resultados de Loteríasde hoy</h1>
       <LotteryCards />
     </div>
   );
