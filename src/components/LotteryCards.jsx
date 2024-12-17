e5b1243d58d91cde2e7e86874cfbc5235a25ee29
Anthony Morel <anthony_morel@hotmail.com>
2024-12-17 15:51:27 -0400

    Nuevo verdes

--- src/components/LotteryCards.jsx
+++ src/components/LotteryCards.jsx
@@ -77,7 +77,7 @@ const LotteryCards = () => {
             key={index}
             className="bg-white shadow-md rounded-lg p-4 border border-gray-200 hover:shadow-lg transition"
           >
-            <h2 className="text-lg font-bold text-green-600 mb-2">{lottery.name}</h2>
+            <h2 className="text-lg font-bold text-teal-600 mb-2">{lottery.name}</h2>
             <p className="text-gray-700">
               <span className="font-medium">Fecha:</span> {lottery.date}
             </p>
