d235b6b359b5d14af695f492a0b3716936f48bfb
Anthony Morel <anthony_morel@hotmail.com>
2025-01-09 17:29:21 -0400

    Salto de linea

--- src/components/LotteryCards.jsx
+++ src/components/LotteryCards.jsx
@@ -62,6 +62,7 @@ const LotteryCards = () => {
       <h1 className=" block text-xl font-medium text-gray-700">
       Resultados de Loteria Nacional, Leidsa, Loteria Real, La Primera, La Suerte, Loteka LotoDom, New York y Florida
       </h1>
+      <hr />
         <label htmlFor="date" className="block text-lg font-medium text-gray-700">
           Buscar por Fecha: 
         </label>
